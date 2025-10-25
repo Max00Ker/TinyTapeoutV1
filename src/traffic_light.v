13ff8f39257a314d1ca8f94820fc097fbd60266f
LAPTOP_MAX\maxik <maximilian.kernmaier@gmail.com>
2025-10-25 10:43:54 +0200

    changes

--- src/traffic_light.v
+++ src/traffic_light.v
@@ -1,4 +1,4 @@
-module tt_um_Max00Ker_traffic_light (
+module tt_um_Max00Ker_Traffic_Light (
     input  wire [7:0] ui_in,    // Dedicated inputs
     output wire [7:0] uo_out,   // Dedicated outputs
     input  wire [7:0] uio_in,   // IOs: input path
