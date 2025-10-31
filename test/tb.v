2700c83cbcb6923105147545ff83dd098081d7d0
LAPTOP_MAX\maxik <maximilian.kernmaier@gmail.com>
2025-10-31 20:51:44 +0100

    changes

--- test/tb.v
+++ test/tb.v
@@ -28,7 +28,7 @@ module tb ();
 `endif
 
   // Replace tt_um_example with your module name:
-  tt_um_Max00Ker_Traffic_Light user_project (
+  tt_um_Max00Ker_Traffic_Light_Top user_project (
 
       // Include power ports for the Gate Level test:
 `ifdef GL_TEST
